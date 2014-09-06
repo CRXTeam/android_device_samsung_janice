375a012bfb6f731b40eabdd09893c5e223a88ab5
Michał <mimirower45@wp.pl>
2014-09-06 20:48:30 +0200

    Add storage fix

--- patches/apply.sh
+++ patches/apply.sh
@@ -4,6 +4,12 @@ MYABSPATH=$(readlink -f "$0")
 PATCHBASE=$(dirname "$MYABSPATH")
 CMBASE=$(readlink -f "$PATCHBASE/../../../../")
 
+echo -e $CL_BLU"Cherrypicking vold patch to allow switching storages"$CL_RST
+cd system/vold
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_system_vold refs/changes/15/56515/2
+git cherry-pick FETCH_HEAD
+cd ../..
+
 for i in $(find "$PATCHBASE"/* -type d); do
 	PATCHNAME=$(basename "$i")
 	PATCHTARGET=$PATCHNAME
