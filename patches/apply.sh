5cd96aac9f25d28d7ba7650b6e99a07c1e69eedd
Jenkins <cr3pt@crxteam.info>
2014-10-28 16:47:38 +0100

    Updates 	modified:   README.md 	deleted:    cm.mk 	modified:   patches/apply.sh 	new file:   patches/frameworks_av/0001-last-commit-STE-OMX: fix getSupportedProfileLevel.patch 	new file:   patches/frameworks_av/0002-last-commit-video-streaming-fix.patch 	renamed:    patches/frameworks_av/0001.patch -> patches/frameworks_av/0003-STE-Multimedia-Fix.patch 	renamed:    patches/frameworks_native/0001.patch -> patches/frameworks_native/0001-last-commit-STE Multimedia Fix.patch 	deleted:    patches/packages_services_Telephony/0001.patch 	new file:   patches/system_vold/0001-vold-allow-pre-configured-device.patch 	deleted:    vendorsetup.sh

--- patches/apply.sh
+++ patches/apply.sh
@@ -4,17 +4,6 @@ MYABSPATH=$(readlink -f "$0")
 PATCHBASE=$(dirname "$MYABSPATH")
 CMBASE=$(readlink -f "$PATCHBASE/../../../../")
 
-echo -e $CL_BLU"Cherrypicking vold patch to allow switching storages"$CL_RST
-cd system/vold
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_system_vold refs/changes/15/56515/2
-git cherry-pick FETCH_HEAD
-cd ../..
-echo -e $CL_BLU"Cherrypicking ART compatibility fix with GCC 4.8"$CL_RST
-cd art
-git fetch https://github.com/JustArchi/android_art cm-11.0
-git cherry-pick 71a0ca3057cc3865bd8e41dcb94443998d028407
-cd ..
-
 for i in $(find "$PATCHBASE"/* -type d); do
 	PATCHNAME=$(basename "$i")
 	PATCHTARGET=$PATCHNAME
