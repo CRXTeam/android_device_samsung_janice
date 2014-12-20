6e07523bb70a67768abd2e94085041c6b721fa5f
zodex <mimirower45@wp.pl>
2014-12-20 09:24:49 +0100

    Fixes for OMX

--- patches/apply.sh
+++ patches/apply.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+echo -e $CL_BLU"Cherrypicking for android_frameworks_av - STE Multimedia Fix [1/4]"$CL_RST
+cd frameworks/av
+git fetch https://github.com/TeamCanjica/android_frameworks_av cm-11.0
+git cherry-pick 87618c1ea54009c2e5e5dfb60060f9cc2e9bcc52
+cd ../..
+
 MYABSPATH=$(readlink -f "$0")
 PATCHBASE=$(dirname "$MYABSPATH")
 CMBASE=$(readlink -f "$PATCHBASE/../../../../")
